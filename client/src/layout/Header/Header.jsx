c31c9ba5b04b19d7cc8d1dc760fb8be085bf57b3
unknown <nard.calma@gmail.com>
2023-09-19 20:16:45 -0700

    feat(Nav): Add navigation component
    Add navigation component, add font family

--- client/src/layout/Header/Header.jsx
+++ client/src/layout/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { Navigation } from "./components";
 import "./styles.css"
 
 const Header = () => {
     return(
         <header>
-            <h1 className="companyName">Ryznrd</h1>
+            <h1 className="companyName">RYZNRD</h1>
+            <Navigation />
         </header>
     )
 }
---
+++ client/src/layout/Header/components/Navigation.jsx
@@ -0,0 +1,13 @@
+const Navigation = () => {
+    return(
+        <nav>
+            <ul className="navList">
+                <li className="navListItem">Home</li>
+                <li className="navListItem">Contact</li>
+                <li className="navListItem">About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
