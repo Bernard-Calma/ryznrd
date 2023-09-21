260a6db8164df8bbb98e4afd06fe06fee82bb6c4
unknown <nard.calma@gmail.com>
2023-09-20 23:23:36 -0700

    feat(Footer): Create footer component
    Create footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./layout/Footer/components/Footer";
 import Header from "./layout/Header/Header";
 
 const App = () => {
   return(
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
