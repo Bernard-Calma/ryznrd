fe34f5d28b8a91842bc1968bd162f257c15e34d6
unknown <nard.calma@gmail.com>
2024-07-14 18:26:43 -0700

    style(Services): Add style in services
    Added simple style to elevate diferent services

--- client/src/pages/home/components/Services.jsx
+++ client/src/pages/home/components/Services.jsx
@@ -1,13 +1,15 @@
 const Services = () => {
     return(
         <section className="sectionServices" id="sectionServices">
-            <p className="sectionTitle">Services</p>
-            <p>Custom PC build - $50</p>
-            <p>Full Cleaning w/ Thermal Paste replacement - $50</p>
-            <p>Windows Installation (Pre-Actiavted) - $50</p>
-            <p>Parts Upgrade/Replacement - $50</p>
-            <p>Repair & Troubleshooting - $50</p>
-
+            <p className="sectionTitle">SERVICES</p>
+            <div className="columnList">
+                <p>Custom PC build - $50</p>
+                <p>Full Cleaning w/ Thermal Paste replacement - $50</p>
+                <p>Windows Installation (Pre-Actiavted) - $50</p>
+                <p>Parts Upgrade/Replacement - $50</p>
+                <p>Repair & Troubleshooting - $50</p>
+                
+            </div>
             <p>*Home service will be $50 more on top of any services</p>
         </section>
     )
