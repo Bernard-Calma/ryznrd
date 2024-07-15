da21d617def12dab387ad5543db1068bdc7e3278
unknown <nard.calma@gmail.com>
2024-07-14 17:55:10 -0700

    fix(Section): Fix section overlapping when resizing browser
    Change height to min-height on frontPage

--- client/src/layout/Main/Main.jsx
+++ client/src/layout/Main/Main.jsx
@@ -1,8 +1,6 @@
 import "../../assets/images/RyznrdLogo.jpg"
 import AvailableBuilds from "../../pages/availableBuilds/AvailableBuilds";
 import Home from "../../pages/home/Home";
-import { GalleryPreview } from "../../pages/home/components";
-import Services from "../../pages/home/components/Services";
 
 const Main = () => {
     return(
